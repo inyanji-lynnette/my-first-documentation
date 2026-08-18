3023c21212a9083a75162e6acdeed728e0f4e5ac
Inyanji Lynnette |  NetSec <wafulalynnette@gmail.com>
2026-08-18 21:48:01 +0300

    correcting the base url failure

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/inyanji-lynnette',
+  url: 'https://inyanji-lynnette.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/my-first-documentation/',
